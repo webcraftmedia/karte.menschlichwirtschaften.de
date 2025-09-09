0ed3d942762ade0b8256869be6c47b213b73fb4c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-09-09 23:14:57 +0200

    initial commit

---
+++ seed/manual/type-ui-components.sql
@@ -0,0 +1,66 @@
+-- Type: text+gallery
+INSERT INTO public."types_profileTemplate" (collection, id, item, sort, types_id)
+SELECT
+  'texts', '1', '7c25fdf6-d5f2-425a-9a2e-03c5297d10bc' , '1', types.id
+FROM
+  public.types as types
+WHERE
+  name = 'text+gallery'
+ON CONFLICT (id) DO UPDATE
+  SET collection = excluded.collection,
+      item = excluded.item,
+      sort = excluded.sort,
+      types_id = excluded.types_id;
+
+INSERT INTO public."types_profileTemplate" (collection, id, item, sort, types_id)
+SELECT
+  'gallery', '2', 'ea97b565-037c-4d0c-bcec-5e38793a6e7f' , '2', types.id
+FROM
+  public.types as types
+WHERE
+  name = 'text+gallery'
+ON CONFLICT (id) DO UPDATE
+  SET collection = excluded.collection,
+      item = excluded.item,
+      sort = excluded.sort,
+      types_id = excluded.types_id;
+
+-- Type: event
+INSERT INTO public."types_profileTemplate" (collection, id, item, sort, types_id)
+SELECT
+  'startEnd', '1', '0b5e5f0a-76a4-407f-84ab-2fd718965909' , '1', types.id
+FROM
+  public.types as types
+WHERE
+  name = 'event'
+ON CONFLICT (id) DO UPDATE
+  SET collection = excluded.collection,
+      item = excluded.item,
+      sort = excluded.sort,
+      types_id = excluded.types_id;
+
+INSERT INTO public."types_profileTemplate" (collection, id, item, sort, types_id)
+SELECT
+  'texts', '2', '94f6af1d-77e5-49ed-937b-6b9addc4f8ac' , '2', types.id
+FROM
+  public.types as types
+WHERE
+  name = 'event'
+ON CONFLICT (id) DO UPDATE
+  SET collection = excluded.collection,
+      item = excluded.item,
+      sort = excluded.sort,
+      types_id = excluded.types_id;
+
+INSERT INTO public."types_profileTemplate" (collection, id, item, sort, types_id)
+SELECT
+  'gallery', '3', 'b0c52d6e-b3d2-4e3b-89e2-065be324e27b' , '3', types.id
+FROM
+  public.types as types
+WHERE
+  name = 'event'
+ON CONFLICT (id) DO UPDATE
+  SET collection = excluded.collection,
+      item = excluded.item,
+      sort = excluded.sort,
+      types_id = excluded.types_id;
